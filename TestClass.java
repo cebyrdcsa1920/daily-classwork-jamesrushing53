abd966c34d137d894625136a4dc0eada4262dd8b
jamesrushing53 <james@erushing.com>
2019-08-26 14:12:19 +0000

    Make week 3 folder and student class

--- TestClass.java
+++ TestClass.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+
 public class TestClass
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is you favorite food? ");
+        String food = keyboard.nextLine();
+        System.out.println("Your favorite food is " + food);
+        keyboard.close();
+    }
+}
