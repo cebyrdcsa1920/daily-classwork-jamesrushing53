139dd9d69a5c968927b610da4acec9bf9bf4cf9c
jamesrushing53 <james@erushing.com>
2019-09-05 14:10:11 +0000

    Practiced making constructor after test

---
+++ week4/James.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+
+public class James
+{
+    private String fullName;
+    private Color color;
+    private int age;
+    private double weight;
+
+    public James()
+    {
+        fullName = "James Troy Rushing";
+        color = Color.WHITE;
+        age = 17;
+        weight = 235.0;
+    }
+
+    public String toString()
+    {
+        return "This is James!";
+    }
+}
---
+++ week4/JamesRunner.java
@@ -0,0 +1,8 @@
+public class JamesRunner
+{
+    public static void main(String[] args)
+    {
+        James myJames = new James();
+        System.out.println(myJames);
+    }
+}
