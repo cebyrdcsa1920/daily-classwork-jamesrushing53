cbd7804cb217d6c22488b26418162ffa690ef335
jamesrushing53 <james@erushing.com>
2019-09-11 14:15:09 +0000

    Create new car

--- week4/CarRunner.java
+++ week4/CarRunner.java
@@ -3,6 +3,14 @@ public class CarRunner
     public static void main(String[] args)
     {
         Car myMustang = new Car();
-        System.out.println(myMustang);
+        //System.out.println(myMustang);
+        myMustang.drive(5.5);
+        System.out.println(myMustang.getAmountOfGas());
+        myMustang.drive(100.5);
+        System.out.println(myMustang.getAmountOfGas());
+        myMustang.fillUpGasTank(9);
+        System.out.println(myMustang.getAmountOfGas());
+
+        Car myJeep = new Car();
     }
 }
